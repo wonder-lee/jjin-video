321e9076ab7860f10eda5e582aaea74b1ec0fd51
wonder-lee <hi.wonder.dev@gmail.com>
2022-11-09 11:59:53 +0900

    google analytics 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,32 @@
 import type { AppProps } from "next/app";
+import Script from "next/script";
 import { NextUIProvider } from "@nextui-org/react";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const GA_ID = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID;
   return (
-    <NextUIProvider>
-      <Component {...pageProps} />
-    </NextUIProvider>
+    <>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
+      <NextUIProvider>
+        <Component {...pageProps} />
+      </NextUIProvider>
+    </>
   );
 }
